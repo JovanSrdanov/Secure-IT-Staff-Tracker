17ad091623c29bc0dfdb90d2d03e10617d861da1
Your Name <srdanov.ra145.2019@uns.ac.rs>
2023-04-07 21:08:02 +0200

    Particles and appbar

--- PKI/pki-frontend/src/App.js
+++ PKI/pki-frontend/src/App.js
@@ -1,8 +1,52 @@
+import ParticlesBg from 'particles-bg'
+import "./particles.css"
+import {AppBar, Box, Button, Toolbar} from "@mui/material";
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import KeyIcon from '@mui/icons-material/Key';
+import Typography from '@mui/material/Typography';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+
 function App() {
     return (
-        <div className="App">
-            Hello!
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <div className="App">
+                <ParticlesBg color="#00FFFF" type="cobweb" num={200} bg={true}/>
+                <Box>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <KeyIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}}/>
+                            <Typography
+                                variant="h6"
+                                noWrap
+                                component="a"
+                                href="/"
+                                sx={{
+                                    mr: 2,
+                                    display: {xs: 'none', md: 'flex'},
+                                    fontFamily: 'monospace',
+                                    fontWeight: 700,
+                                    letterSpacing: '.3rem',
+                                    color: 'inherit',
+                                    textDecoration: 'none',
+
+                                }}
+                            > PKI
+                            </Typography>
+                            <Button sx={{color: 'inherit'}}>All certificates</Button>
+                            <Button sx={{color: 'inherit'}}>Create certificate</Button>
+                            <Button sx={{color: 'inherit', marginLeft: 'auto'}}>Log out</Button>
+                        </Toolbar>
+                    </AppBar>
+                    {/*OVde ce ici ruter*/}
+                </Box>
+            </div>
+        </ThemeProvider>
     );
 }
 
