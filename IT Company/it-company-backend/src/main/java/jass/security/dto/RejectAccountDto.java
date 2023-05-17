118cc3b7ab675dd6cbfd6b9d2cb5478b3afa3c7f
strale15 <karte32114@gmail.com>
2023-05-17 21:10:25 +0200

    Send email

---
+++ IT Company/it-company-backend/src/main/java/jass/security/dto/RejectAccountDto.java
@@ -0,0 +1,15 @@
+package jass.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RejectAccountDto {
+    String mail;
+    String reason;
+}
