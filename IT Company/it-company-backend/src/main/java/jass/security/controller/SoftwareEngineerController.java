999ea15384c66c9fcf5229344d8619c468cdada4
stojanovic00 <al.stojanovic0@gmail.com>
2023-05-19 16:28:27 +0200

    Merge me

--- IT Company/it-company-backend/src/main/java/jass/security/controller/SoftwareEngineerController.java
+++ IT Company/it-company-backend/src/main/java/jass/security/controller/SoftwareEngineerController.java
@@ -1,9 +1,25 @@
 package jass.security.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jass.security.dto.swengineer.AddSkillDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("sw-engineer")
 public class SoftwareEngineerController {
+    @PostMapping("skill")
+    @PreAuthorize("hasAuthority('addSkillSwEngineer')")
+    public ResponseEntity<?> AddSkill(@RequestBody AddSkillDto dto, Principal principal){
+        return null;
+    }
+
+    @GetMapping("skill/{id}")
+    @PreAuthorize("hasAuthority('removeSkillSwEngineer')")
+    public ResponseEntity<?> RemoveSkill(@PathVariable("id") UUID skillId, Principal principal){
+        return null;
+    }
 }
