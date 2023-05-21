44131e1aae96feb320bf26effb041196fb4c9cef
stefanapostolovic <93873256+stefanapostolovic@users.noreply.github.com>
2023-05-21 20:01:33 +0200

    added validation for update logged-in user info

--- IT Company/it-company-backend/src/main/java/jass/security/dto/employee/EmployeeProfileInfoDto.java
+++ IT Company/it-company-backend/src/main/java/jass/security/dto/employee/EmployeeProfileInfoDto.java
@@ -1,5 +1,8 @@
 package jass.security.dto.employee;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jass.security.dto.AddressDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,9 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeProfileInfoDto {
+    @NotBlank(message = "The name is required.")
     private String name;
+    @NotBlank(message = "The surname is required.")
     private String surname;
+    @NotBlank(message = "The phoneNumber is required.")
     private String phoneNumber;
+    @NotBlank(message = "The profession is required.")
     private String profession;
+    @Valid
+    @NotNull
     private AddressDto address;
 }
