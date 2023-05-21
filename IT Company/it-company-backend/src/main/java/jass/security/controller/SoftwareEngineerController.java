8d82049c7ea6d2a7190ef761f3973880e3dbe153
stefanapostolovic <93873256+stefanapostolovic@users.noreply.github.com>
2023-05-21 19:43:26 +0200

    removed unnecessary endpoints

--- IT Company/it-company-backend/src/main/java/jass/security/controller/SoftwareEngineerController.java
+++ IT Company/it-company-backend/src/main/java/jass/security/controller/SoftwareEngineerController.java
@@ -1,17 +1,13 @@
 package jass.security.controller;
 
-import jass.security.dto.employee.EmployeeProfileInfoDto;
 import jass.security.dto.swengineer.AddSkillDto;
 import jass.security.exception.NotFoundException;
 import jass.security.model.Account;
 import jass.security.model.Skill;
-import jass.security.model.SoftwareEngineer;
 import jass.security.service.interfaces.IAccountService;
-import jass.security.service.interfaces.IEmployeeService;
 import jass.security.service.interfaces.ISoftwareEngineerService;
 import jass.security.utils.ObjectMapperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -32,56 +28,6 @@ public class SoftwareEngineerController {
                                       ISoftwareEngineerService softwareEngineerService) {
         this._accountService = _accountService;
         _softwareEngineerService = softwareEngineerService;
-    }
-
-    @GetMapping("/logged-in-info")
-    @PreAuthorize("hasAuthority('getLoggedInInfo')")
-    public ResponseEntity<?> getLoggedInInfo(Principal principal) {
-        String employeeEmail = principal.getName();
-        Account employeeCredentials = _accountService.findByEmail(employeeEmail);
-
-        try {
-            SoftwareEngineer employeeInfo = _softwareEngineerService.findById(employeeCredentials.getEmployeeId());
-            return new ResponseEntity<>(
-                    ObjectMapperUtils.map(employeeInfo, EmployeeProfileInfoDto.class),
-                    HttpStatus.OK
-            );
-        } catch (NotFoundException e) {
-            return new ResponseEntity<>(
-                    "engineer not found",
-                    HttpStatus.NOT_FOUND
-            );
-        }
-    }
-
-    @PutMapping("/logged-in-info")
-    @PreAuthorize("hasAuthority('updateLoggedInInfo')")
-    public ResponseEntity<?> updateLoggedInInfo(@RequestBody EmployeeProfileInfoDto dto, Principal principal) {
-        String employeeEmail = principal.getName();
-        Account employeeCredentials = _accountService.findByEmail(employeeEmail);
-
-        try {
-            var updatedInfo = _softwareEngineerService.update(employeeCredentials.getEmployeeId(), dto);
-            return new ResponseEntity<>(
-                    ObjectMapperUtils.map(updatedInfo, EmployeeProfileInfoDto.class),
-                    HttpStatus.OK
-            );
-        } catch (NotFoundException e) {
-            return new ResponseEntity<>(
-                    "cannot find employee info",
-                    HttpStatus.NOT_FOUND
-            );
-        } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(
-                    "failed to update info, argument is null",
-                    HttpStatus.BAD_REQUEST
-            );
-        } catch (OptimisticLockingFailureException e) {
-            return new ResponseEntity<>(
-                    "failed to update info due to optimistic locking",
-                    HttpStatus.INTERNAL_SERVER_ERROR
-            );
-        }
     }
 
     @GetMapping("skill")
