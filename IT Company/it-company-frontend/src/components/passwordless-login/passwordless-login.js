cebe54b3349981dcc6ce9e6b2bdcfcb058b98865
Your Name <srdanov.ra145.2019@uns.ac.rs>
2023-05-22 15:07:32 +0200

    Getting params from url

--- IT Company/it-company-frontend/src/components/passwordless-login/passwordless-login.js
+++ IT Company/it-company-frontend/src/components/passwordless-login/passwordless-login.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import {useParams} from "react-router-dom";
+import React, {useEffect} from 'react';
+import {useNavigate, useParams} from "react-router-dom";
+
+// import interceptor from "../../interceptor/interceptor";
 
 function PasswordlessLogin(props) {
     const params = useParams();
+    const navigate = useNavigate();
 
-    console.log(params);
+
+    useEffect(() => {
+        console.log(params.hash);
+        // interceptor.post("").then((res) => {
+        //     console.log(res.data)
+        //     navigate("/profile")
+        // }).catch((err) => {
+        //     console.log(err)
+        // alert("Can not log in with this hash code.")
+        //     navigate("/login")
+        // })
+    }, []);
+
+
     return (
         <div></div>
     );
