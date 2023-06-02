fa77443b134247a1ffe37381dea324bde8906b2f
stojanovic00 <al.stojanovic0@gmail.com>
2023-06-02 14:27:52 +0200

    GDPR data encryption

--- IT Company/it-company-backend/src/main/java/jass/security/model/Employee.java
+++ IT Company/it-company-backend/src/main/java/jass/security/model/Employee.java
@@ -3,6 +3,7 @@ package jass.security.model;
 import jakarta.persistence.*;
 import jass.security.dto.employee.EmployeeProfileInfoDto;
 import lombok.*;
+import org.hibernate.annotations.ColumnTransformer;
 
 import java.util.UUID;
 
@@ -16,15 +17,30 @@ import java.util.UUID;
 public class Employee {
     @Id
     private UUID id;
-    @Column(nullable = false)
+
+
+    @ColumnTransformer( read = "pgp_sym_decrypt( name, current_setting('encrypt.key') )",
+            write = " pgp_sym_encrypt( ?, current_setting('encrypt.key') )")
+    @Column(nullable = false, columnDefinition = "bytea")
     private String name;
-    @Column(nullable = false)
+
+    @ColumnTransformer( read = "pgp_sym_decrypt( surname, current_setting('encrypt.key') )",
+            write = " pgp_sym_encrypt( ?, current_setting('encrypt.key') )")
+    @Column(nullable = false, columnDefinition = "bytea")
     private String surname;
+
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Address address;
-    @Column(nullable = false)
+
+    @ColumnTransformer( read = "pgp_sym_decrypt( phone_number, current_setting('encrypt.key') )",
+            write = " pgp_sym_encrypt( ?, current_setting('encrypt.key') )")
+    @Column(nullable = false, columnDefinition = "bytea")
     private String phoneNumber;
-    @Column(nullable = false)
+
+
+    @ColumnTransformer( read = "pgp_sym_decrypt( profession, current_setting('encrypt.key') )",
+            write = " pgp_sym_encrypt( ?, current_setting('encrypt.key') )")
+    @Column(nullable = false, columnDefinition = "bytea")
     private String profession; //zvanje
 
     public void update(EmployeeProfileInfoDto dto) {
--- IT Company/it-company-backend/src/main/java/jass/security/model/Project.java
+++ IT Company/it-company-backend/src/main/java/jass/security/model/Project.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnTransformer;
 
 import java.util.UUID;
 
@@ -18,8 +19,12 @@ import java.util.UUID;
 public class Project {
     @Id
     private UUID id;
-    @Column(nullable = false)
+
+    @ColumnTransformer( read = "pgp_sym_decrypt( name, current_setting('encrypt.key') )",
+            write = " pgp_sym_encrypt( ?, current_setting('encrypt.key') )")
+    @Column(nullable = false, columnDefinition = "bytea")
     private String name;
+
     @Column(nullable = false)
     private DateRange duration;
 
