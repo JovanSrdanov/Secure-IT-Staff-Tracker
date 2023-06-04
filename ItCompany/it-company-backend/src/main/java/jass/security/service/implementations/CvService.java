4e9936260790811f4f0efdeaf3164b7290bf01aa
stojanovic00 <al.stojanovic0@gmail.com>
2023-06-04 19:19:12 +0200

    Cv encryption

--- ItCompany/it-company-backend/src/main/java/jass/security/service/implementations/CvService.java
+++ ItCompany/it-company-backend/src/main/java/jass/security/service/implementations/CvService.java
@@ -1,19 +1,59 @@
 package jass.security.service.implementations;
 
+import jass.security.dto.CvAesDto;
+import jass.security.exception.NotFoundException;
+import jass.security.model.Cv;
+import jass.security.model.SoftwareEngineer;
+import jass.security.repository.ICvRepository;
+import jass.security.repository.ISwEngineerRepository;
 import jass.security.service.interfaces.ICvService;
+import jass.security.utils.Base64Utility;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.UUID;
 
 @Service
 @Primary
 public class CvService implements ICvService {
+    private final SecretKey secretKey;
+    private final int blockSize;
+    private final String algorithm;
+    private final ISwEngineerRepository swEngineerRepository;
+    private final ICvRepository cvRepository;
+public CvService(@Value("${cvAesKey}") String secretKeyStr, ICvRepository cvRepository, ISwEngineerRepository swEngineerRepository, ICvRepository cvRepository1){
+
+    this.secretKey = stringToSecretKey(secretKeyStr);
+        this.swEngineerRepository = swEngineerRepository;
+    this.cvRepository = cvRepository1;
+    this.blockSize = 16;
+        this.algorithm = "AES/CBC/PKCS5Padding";
+    }
+
+
+    private SecretKey stringToSecretKey(String secretKeyStr) {
+        byte[] secretKeyByte;
+        try {
+            secretKeyByte= Base64Utility.decode(secretKeyStr);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new SecretKeySpec(secretKeyByte, 0, secretKeyByte.length, "AES");
+    }
 
     private String getCvDir(){
         String runtimeDir = getClass().getResource("").getPath();
@@ -22,14 +62,79 @@ public class CvService implements ICvService {
     }
 
     @Override
-    public void save(MultipartFile file, UUID engineerId) throws IOException {
-        String fileName = "cv_" + engineerId.toString();
-        file.transferTo(new File(getCvDir() + fileName));
+    public void save(MultipartFile file, UUID engineerId) throws IOException, NotFoundException {
+        CvAesDto dto = encrypt(file.getBytes());
+        String filePath = getCvDir() + "cv_" + engineerId.toString();
+
+        try(FileOutputStream fos = new FileOutputStream(filePath)){
+           fos.write(dto.getEncryptedCv());
+           fos.flush();
+        }
+        catch (IOException e){
+           throw new RuntimeException(e.getMessage());
+        }
+
+        Cv cv = new Cv(UUID.randomUUID(), dto.getAesInitVector());
+        cvRepository.save(cv);
+
+        //Linking cv to engineer
+        var engineerResult =  swEngineerRepository.findById(engineerId);
+        if(engineerResult.isEmpty()){
+            throw new NotFoundException("Engineer not found");
+        }
+        SoftwareEngineer engineer = engineerResult.get();
+        engineer.setCv(cv);
+        swEngineerRepository.save(engineer);
     }
 
     @Override
-    public byte[] read(UUID engineerId) throws IOException {
-        String fileName = "cv_" + engineerId.toString();
-        return Files.readAllBytes(Path.of(getCvDir() + fileName));
+    public byte[] read(UUID engineerId) throws IOException, NotFoundException {
+        String filePath = getCvDir() + "cv_" + engineerId.toString();
+
+
+        var engineerResult =  swEngineerRepository.findById(engineerId);
+        if(engineerResult.isEmpty()){
+            throw new NotFoundException("Engineer not found");
+        }
+        byte[] aesInitVector = engineerResult.get().getCv().getAesInitVector();
+        byte[] encryptedCv =  Files.readAllBytes(Path.of(filePath));
+
+        return decrypt(new CvAesDto(encryptedCv, aesInitVector));
     }
+
+    @Override
+    public CvAesDto encrypt(byte[] cv) {
+        try {
+            IvParameterSpec iv = generateIv();
+            Cipher cipher = null;
+            cipher = Cipher.getInstance(algorithm);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+            byte[] encryptedCv = cipher.doFinal(cv);
+            return new CvAesDto(encryptedCv, iv.getIV());
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException |
+                 IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public byte[] decrypt(CvAesDto dto) {
+        try{
+        IvParameterSpec iv = new IvParameterSpec(dto.getAesInitVector());
+        Cipher cipher = null;
+        cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, iv);
+        return cipher.doFinal(dto.getEncryptedCv());
+        } catch (InvalidAlgorithmParameterException | NoSuchPaddingException | IllegalBlockSizeException |
+                 NoSuchAlgorithmException | BadPaddingException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public IvParameterSpec generateIv() {
+        byte[] iv = new byte[blockSize];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
 }
--- ItCompany/it-company-backend/src/main/java/jass/security/service/interfaces/ICvService.java
+++ ItCompany/it-company-backend/src/main/java/jass/security/service/interfaces/ICvService.java
@@ -1,11 +1,15 @@
 package jass.security.service.interfaces;
 
+import jass.security.dto.CvAesDto;
+import jass.security.exception.NotFoundException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.UUID;
 
 public interface ICvService {
-    void save(MultipartFile file, UUID swEngineerId) throws IOException;
-    byte[] read(UUID engineerId) throws IOException;
+    void save(MultipartFile file, UUID swEngineerId) throws IOException, NotFoundException;
+    byte[] read(UUID engineerId) throws IOException, NotFoundException;
+    CvAesDto encrypt(byte[] cv);
+    byte[] decrypt(CvAesDto dto);
 }
