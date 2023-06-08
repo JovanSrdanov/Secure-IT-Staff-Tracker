05588f8126111b7f070b7b023af5b9b7ca1c0705
Your Name <srdanov.ra145.2019@uns.ac.rs>
2023-06-08 19:26:33 +0200

    Notifications for sus events

--- ItCompany/it-company-backend/src/main/java/jass/security/controller/PrivilegeController.java
+++ ItCompany/it-company-backend/src/main/java/jass/security/controller/PrivilegeController.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +25,9 @@ public class PrivilegeController {
     private final IPrivilegeService privilegeService;
     private final IRoleService roleService;
     private final IAdministratorService administratorService;
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
 
     //Clicksend
     @Autowired
@@ -41,7 +45,7 @@ public class PrivilegeController {
     public ResponseEntity<?> updatePrivileges(@RequestBody PermissionUpdateRequest dto) {
         roleService.updatePrivileges(dto);
         logger.info("Privileges for the role: " + dto.getRoleName() + " changed");
-
+        this.simpMessagingTemplate.convertAndSend("/socket-publisher", "Privileges for the role: " + dto.getRoleName() + " changed");
         //Clicksend
         var admins = administratorService.findAll();
         if (admins != null) {
