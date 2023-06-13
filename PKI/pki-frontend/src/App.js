7573a895582f743c9b4b38343be1c239ce513493
Your Name <srdanov.ra145.2019@uns.ac.rs>
2023-06-13 18:42:45 +0200

    Removing uneccessery files

--- PKI/pki-frontend/src/App.js
+++ PKI/pki-frontend/src/App.js
@@ -5,7 +5,7 @@ import KeyIcon from '@mui/icons-material/Key';
 import Typography from '@mui/material/Typography';
 import {Navigate, Route, Routes, useNavigate} from "react-router-dom";
 import LoginPage from "./pages/login-page/login-page";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import ParticlesBg from 'particles-bg'
 import ChangePasswordPage from "./pages/certificate-user-change-password-page/change-password-page";
 import HackerHeaders from "./components/hacker-headers/hackerHeaders";
@@ -13,7 +13,7 @@ import AllCertificatesPage from "./pages/admin-pages/all-certificates-page";
 import CreateCertificatePage from "./pages/admin-pages/create-certificate-page";
 import MyCertificatesPage from "./pages/certificate-user-pages/my-certificates-page";
 import IssueCertificatePage from "./pages/certificate-user-pages/issue-certificate-page";
-import { useKeycloak } from "@react-keycloak/web";
+import {useKeycloak} from "@react-keycloak/web";
 
 const darkTheme = createTheme({
     palette: {
@@ -29,10 +29,10 @@ function App() {
     const navigate = useNavigate();
 
     // Keycloak
-    const { keycloak, initialized } = useKeycloak();
+    const {keycloak, initialized} = useKeycloak();
 
     function isAuthenticatedUsingKeycloak() {
-      return keycloak && keycloak?.token;
+        return keycloak && keycloak?.token;
     }
 
     keycloak.onTokenExpired = () => {
@@ -89,42 +89,41 @@ function App() {
             //setRole("ROLE_PKI_ADMIN");
             localStorage.setItem("jwt", keycloak?.token);
             return "ROLE_PKI_ADMIN";
-        }
-        else {
+        } else {
             console.log("Nije preko keycloak-a")
             const token = localStorage.getItem("jwt");
             console.log("OBICAN TOKEN: " + token);
             if (!token) {
-              //navigate("/login");
-              //setRole(null);
-              return null;
+                //navigate("/login");
+                //setRole(null);
+                return null;
             }
 
             try {
-              const decoded = JSON.parse(atob(token.split(".")[1]));
-              //console.log(decoded);
-              const currentTime = new Date().getTime() / 1000;
-              if (decoded.exp < currentTime) {
+                const decoded = JSON.parse(atob(token.split(".")[1]));
+                //console.log(decoded);
+                const currentTime = new Date().getTime() / 1000;
+                if (decoded.exp < currentTime) {
+                    localStorage.removeItem("jwt");
+                    //navigate("/login");
+                    //setRole(null);
+                    return null;
+                }
+                //setRole(decoded.role);
+                console.log("OBICNA ROLA: " + decoded.role)
+                // ovo se desi ako se izlogujem preko keycloaka sa druge aplikacije, ostane keycloak jwt u local
+                // storage-u
+                if (decoded.role === undefined) {
+                    localStorage.removeItem("jwt");
+                    setJwt(null);
+                    return null;
+                }
+                return decoded.role;
+            } catch (error) {
                 localStorage.removeItem("jwt");
                 //navigate("/login");
                 //setRole(null);
                 return null;
-              }
-              //setRole(decoded.role);
-              console.log("OBICNA ROLA: " + decoded.role)
-              // ovo se desi ako se izlogujem preko keycloaka sa druge aplikacije, ostane keycloak jwt u local
-              // storage-u
-              if (decoded.role === undefined) {
-                localStorage.removeItem("jwt");
-                setJwt(null);
-                return null;
-              }
-              return decoded.role;
-            } catch (error) {
-              localStorage.removeItem("jwt");
-              //navigate("/login");
-              //setRole(null);
-              return null;
             }
         }
     }
@@ -135,8 +134,7 @@ function App() {
         //setRole(null);
         if (isAuthenticatedUsingKeycloak()) {
             keycloak.logout();
-        }
-        else {
+        } else {
             navigate("/login");
         }
     };
@@ -149,136 +147,136 @@ function App() {
 
 
     return (
-      <ThemeProvider theme={darkTheme}>
-        <ParticlesBg color="#008B8B" type="cobweb" num={200} bg={true} />
-        <div className="App">
-          <Box>
-            <AppBar position="static">
-              <Toolbar>
-                <KeyIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-                <Typography
-                  variant="h6"
-                  noWrap
-                  sx={{
-                    caretColor: "transparent",
-                    mr: 2,
-                    display: { xs: "none", md: "flex" },
-                    fontFamily: "monospace",
-                    fontWeight: 700,
-                    letterSpacing: ".3rem",
-                    color: "inherit",
-                    textDecoration: "none",
-                  }}
-                >
-                  PKI
-                </Typography>
-                {ROLE === "ROLE_PKI_ADMIN" && (
-                  <>
-                    <Button
-                      sx={{ color: "inherit" }}
-                      onClick={() => navigate("/all-certificates")}
-                    >
-                      All certificates
-                    </Button>
-                    <Button
-                      sx={{ color: "inherit" }}
-                      onClick={() => navigate("/create-certificate")}
-                    >
-                      Create certificate
-                    </Button>
-                  </>
-                )}
-                {ROLE === "ROLE_CERTIFICATE_USER" && (
-                  <>
-                    <Button
-                      sx={{ color: "inherit" }}
-                      onClick={() => navigate("/my-certificates")}
-                    >
-                      My certificates
-                    </Button>
-                    <Button
-                      sx={{ color: "inherit" }}
-                      onClick={() => navigate("/issue-certificate")}
-                    >
-                      Issue a certificate
-                    </Button>
-                  </>
-                )}
-                {ROLE === "ROLE_CERTIFICATE_USER_CHANGE_PASSWORD" && (
-                  <Button
-                    sx={{ color: "inherit" }}
-                    onClick={() => navigate("/change-password")}
-                  >
-                    Change password{" "}
-                  </Button>
-                )}
-                {ROLE !== null && (
-                  <Button
-                    sx={{ color: "inherit", marginLeft: "auto" }}
-                    onClick={handleLogout}
-                  >
-                    Log out
-                  </Button>
-                )}
-              </Toolbar>
-            </AppBar>
-            <Routes>
-              {/* <Route path="/" element={<Navigate to="/all-certificates"/>}/>
+        <ThemeProvider theme={darkTheme}>
+            <ParticlesBg color="#008B8B" type="cobweb" num={200} bg={true}/>
+            <div className="App">
+                <Box>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <KeyIcon sx={{display: {xs: "none", md: "flex"}, mr: 1}}/>
+                            <Typography
+                                variant="h6"
+                                noWrap
+                                sx={{
+                                    caretColor: "transparent",
+                                    mr: 2,
+                                    display: {xs: "none", md: "flex"},
+                                    fontFamily: "monospace",
+                                    fontWeight: 700,
+                                    letterSpacing: ".3rem",
+                                    color: "inherit",
+                                    textDecoration: "none",
+                                }}
+                            >
+                                PKI
+                            </Typography>
+                            {ROLE === "ROLE_PKI_ADMIN" && (
+                                <>
+                                    <Button
+                                        sx={{color: "inherit"}}
+                                        onClick={() => navigate("/all-certificates")}
+                                    >
+                                        All certificates
+                                    </Button>
+                                    <Button
+                                        sx={{color: "inherit"}}
+                                        onClick={() => navigate("/create-certificate")}
+                                    >
+                                        Create certificate
+                                    </Button>
+                                </>
+                            )}
+                            {ROLE === "ROLE_CERTIFICATE_USER" && (
+                                <>
+                                    <Button
+                                        sx={{color: "inherit"}}
+                                        onClick={() => navigate("/my-certificates")}
+                                    >
+                                        My certificates
+                                    </Button>
+                                    <Button
+                                        sx={{color: "inherit"}}
+                                        onClick={() => navigate("/issue-certificate")}
+                                    >
+                                        Issue a certificate
+                                    </Button>
+                                </>
+                            )}
+                            {ROLE === "ROLE_CERTIFICATE_USER_CHANGE_PASSWORD" && (
+                                <Button
+                                    sx={{color: "inherit"}}
+                                    onClick={() => navigate("/change-password")}
+                                >
+                                    Change password{" "}
+                                </Button>
+                            )}
+                            {ROLE !== null && (
+                                <Button
+                                    sx={{color: "inherit", marginLeft: "auto"}}
+                                    onClick={handleLogout}
+                                >
+                                    Log out
+                                </Button>
+                            )}
+                        </Toolbar>
+                    </AppBar>
+                    <Routes>
+                        {/* <Route path="/" element={<Navigate to="/all-certificates"/>}/>
                         <Route path="/login" element={<LoginPage/>}/> */}
-              {ROLE === "ROLE_PKI_ADMIN" && (
-                <>
-                  <Route
-                    path="/all-certificates"
-                    element={<AllCertificatesPage />}
-                  />
-                  <Route
-                    path="/create-certificate"
-                    element={<CreateCertificatePage />}
-                  />
-                  <Route
-                    path="*"
-                    element={<Navigate to="/all-certificates" />}
-                  />
-                </>
-              )}
-              {ROLE === "ROLE_CERTIFICATE_USER" && (
-                <>
-                  <Route
-                    path="/my-certificates"
-                    element={<MyCertificatesPage />}
-                  />
-                  <Route
-                    path="/issue-certificate"
-                    element={<IssueCertificatePage />}
-                  />
-                  <Route
-                    path="*"
-                    element={<Navigate to="/my-certificates" />}
-                  />
-                </>
-              )}
-              {ROLE === "ROLE_CERTIFICATE_USER_CHANGE_PASSWORD" && (
-                <>
-                  <Route
-                    path="/change-password"
-                    element={<ChangePasswordPage />}
-                  />
-                  <Route
-                    path="*"
-                    element={<Navigate to="/change-password" />}
-                  />
-                </>
-              )}
-              {ROLE === null && (
-                <>
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path="/*" element={<Navigate to="/login" />} />
-                </>
-              )}
-            </Routes>
-          </Box>
-        </div>
-      </ThemeProvider>
+                        {ROLE === "ROLE_PKI_ADMIN" && (
+                            <>
+                                <Route
+                                    path="/all-certificates"
+                                    element={<AllCertificatesPage/>}
+                                />
+                                <Route
+                                    path="/create-certificate"
+                                    element={<CreateCertificatePage/>}
+                                />
+                                <Route
+                                    path="*"
+                                    element={<Navigate to="/all-certificates"/>}
+                                />
+                            </>
+                        )}
+                        {ROLE === "ROLE_CERTIFICATE_USER" && (
+                            <>
+                                <Route
+                                    path="/my-certificates"
+                                    element={<MyCertificatesPage/>}
+                                />
+                                <Route
+                                    path="/issue-certificate"
+                                    element={<IssueCertificatePage/>}
+                                />
+                                <Route
+                                    path="*"
+                                    element={<Navigate to="/my-certificates"/>}
+                                />
+                            </>
+                        )}
+                        {ROLE === "ROLE_CERTIFICATE_USER_CHANGE_PASSWORD" && (
+                            <>
+                                <Route
+                                    path="/change-password"
+                                    element={<ChangePasswordPage/>}
+                                />
+                                <Route
+                                    path="*"
+                                    element={<Navigate to="/change-password"/>}
+                                />
+                            </>
+                        )}
+                        {ROLE === null && (
+                            <>
+                                <Route path="/login" element={<LoginPage/>}/>
+                                <Route path="/*" element={<Navigate to="/login"/>}/>
+                            </>
+                        )}
+                    </Routes>
+                </Box>
+            </div>
+        </ThemeProvider>
     );
 }
 
